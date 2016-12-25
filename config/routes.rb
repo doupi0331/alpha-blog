ad0798c9686ffad31863021e8897cfa27eb9d24e
doupi0331 <lisau4mp6@gmail.com>
2016-12-25 15:45:08 +0800

    create article template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 	#get "pages/home" => "pages#home"
 	get "about" => "pages#about"
-
 	root "pages#home"
+
+	# 會產生所有articles Table所需的path
+	# articles GET    /articles(.:format)          articles#index
+    #          POST   /articles(.:format)          articles#create
+  	# new_article GET    /articles/new(.:format)      articles#new
+	# edit_article GET    /articles/:id/edit(.:format) articles#edit
+    # article GET    /articles/:id(.:format)      articles#show
+    #         PATCH  /articles/:id(.:format)      articles#update
+    #         PUT    /articles/:id(.:format)      articles#update
+    #         DELETE /articles/:id(.:format)      articles#destroy
+	resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
