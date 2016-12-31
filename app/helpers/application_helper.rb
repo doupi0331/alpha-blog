d6e68d93a1e11022275acb25c48ac18efc5c1ea9
doupi0331 <lisau4mp6@gmail.com>
2016-12-31 19:47:58 +0800

    add user show page and create a helper functions for gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def gravatar_for(user,options = {size: 80})
+		# 用email抓取gravatar網站的帳號id
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		# 設定圖片SIZE
+		size = options[:size]
+		# gravatar api網址
+		gravatar_url = "http://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		# 抓取圖片, 圖片顯示名稱為username, css class 為 img-circle
+		image_tag(gravatar_url, alt: user.username, class: "img-circle")
+	end
 end
