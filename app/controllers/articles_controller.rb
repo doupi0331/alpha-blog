494d8111b5a4c80d9cc5f67a2b3703dc76919c6b
doupi0331 <lisau4mp6@gmail.com>
2017-01-05 19:51:13 +0800

    Add edit category feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-
 		 if @article.update(article_params) 
 		 	flash[:success] = "Article was successfully updated"
 		 	redirect_to article_path(@article)
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_admin, except: [:index, :show]
   
 	def index
@@ -20,8 +21,21 @@ class CategoriesController < ApplicationController
   end
 
 	def show
-		@category = Category.find(params[:id])
 		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+	end
+	
+	def edit
+		
+	end
+	
+	def update
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated."
+			redirect_to category_path(@category)
+		else
+			render 'edit'
+		end
+		
 	end
 	
 	private
@@ -33,5 +47,8 @@ class CategoriesController < ApplicationController
 		   flash[:danger] = "Only admins can perform that action"
 		   redirect_to categories_path
 		 end
+	 end
+	 def set_category
+		 @category = Category.find(params[:id])
 	 end
 end
